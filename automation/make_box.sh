816e3c1d78f27eace9bf90b02c6d2db41e949990
jorgegabrielti <jorgegabriel.ti@gmail.com>
2022-04-21 23:03:26 -0300

    [add] script to make a box based Rocky Linux.

---
+++ automation/make_box.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# STEPS:
+- Check distro
+- Update system
+- Install commun packages
+- Install Vbox Guest Additions
+- Configure vagrant user
+- Configure sudoers.d to vagrant user
+- Configure vagrant ssh keys
+- Configure sshd_config
+- Clear the trash files
+- Poweroff the machine
+- Export to pack the vm
+- Make a box
