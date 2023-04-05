70b390a7a035c10c240c55a3894dcba24c943f94
jorgegabrielti <jorgegabriel.ti@gmail.com>
2023-04-05 20:52:40 -0300

    [update] vagrant provision to RockyLinux box

--- Rocky-Linux/provision/provision.sh
+++ Rocky-Linux/provision/provision.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo "Hello-World!"
+echo "Install Docker Engine"
+
+sudo dnf update -y
+sudo dnf check-update
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install -y docker-ce docker-ce-cli containerd.io
+sudo docker --version
+sudo systemctl enable --now docker
+sudo systemctl status docker
+sudo usermod -aG docker $(whoami)
+docker run --rm hello-world
